98c39e52badc4a8584f7009eab9530bc9ec835b2
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2022-08-06 23:44:02 +0100

    test error

--- cypress/e2e/home_page.cy.js
+++ cypress/e2e/home_page.cy.js
@@ -1,5 +1,12 @@
 describe('The Home Page', () => {
   it('successfully loads', () => {
-    cy.visit('/'); // change URL to match your dev URL
+    cy.visit('/');
   });
+
+  // it('clicks the quiz button and asserts the url', () => {
+  //   //click the button
+  //   // cy.get('Quiz');
+  //   //assert the url
+  //   // cy.url().should('include', 'Quiz');
+  // });
 });
