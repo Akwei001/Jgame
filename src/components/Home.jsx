7dee04f86e24746ad322ef88867ddc5772c0d4e0
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2022-08-17 20:35:22 +0100

    card game button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -24,6 +24,15 @@ function Home() {
             to='Hiragana'
             className='block px-8 py-3 text-sm font-medium rounded-full hover:bg-transparent'
           >
+            Card Game
+          </Link>
+        </div>
+
+        <div className='inline-block p-[2px] rounded-full bg-gradient-to-r from-pink-500  to-purple-500 hover:text-white active:text-opacity-75 focus:outline-none focus:ring'>
+          <Link
+            to='Hiragana'
+            className='block px-8 py-3 text-sm font-medium rounded-full hover:bg-transparent'
+          >
             Hiragana
           </Link>
         </div>
