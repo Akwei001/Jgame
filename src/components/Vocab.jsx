1e631a2d6f37ef6324aef762f9e6f6ec7a2d0aa1
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2022-09-06 22:27:10 +0100

    vocab component added

---
+++ src/components/Vocab.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import React from 'react';
+
+function Vocab() {
+  return <div>Vocab</div>;
+}
+
+export default Vocab;
