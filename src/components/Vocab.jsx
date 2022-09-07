e076e0d59e32c4cad64fd4d0e676136e55bdc4c3
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2022-09-07 22:05:10 +0100

    dummy vocab added

--- src/components/Vocab.jsx
+++ src/components/Vocab.jsx
@@ -2,6 +2,16 @@ import React from 'react';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
+const newVocab = [
+  {
+    kanji: '一',
+    English: 'one',
+    Onyomi: ['ichi', 'itsu'],
+    Kunyomi: ['hito(tsu)', 'hito'],
+    kanjiVocab: ['一人 (one person, alone)'],
+  },
+];
+
 import React from 'react';
 
 function Vocab() {
