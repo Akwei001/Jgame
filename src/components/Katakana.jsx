f3ff925bde58ae40b24680d19aee8ff00ff43f93
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2022-09-23 10:23:23 +0100

    data display changed

--- src/components/Katakana.jsx
+++ src/components/Katakana.jsx
@@ -113,22 +113,33 @@ function Katakana() {
 
       <table className='m-10 text-lg bg-gray-200 border border-collapse rounded-lg table-auto'>
         <thead>
-          <tr className='text-white rounded-lg bg-gradient-to-r from-purple-400 to-pink-600'>
-            <th className='p-2 border border-white '>a</th>
+          <tr className='text-black rounded-lg bg-gradient-to-r from-purple-400 to-pink-600'>
+            {/* <th className='p-2 border border-white '>a</th>
             <th className='p-2 border border-white '>b</th>
             <th className='p-2 border border-white '>c</th>
             <th className='p-2 border border-white '>d</th>
-            <th className='p-2 border border-white '>e</th>
+            <th className='p-2 border border-white '>e</th> */}
           </tr>
         </thead>
-
-        <tbody>
+        {ktile.map((tile) => (
+          <tbody className='text-black' key={tile.id}>
+            <th className='p-2 text-sm border border-white '>
+              <th className='p-2'>{tile.katakana}</th>
+              <th className='p-2'>{tile.romaji}</th>
+            </th>
+            <th className='p-2 text-sm border border-white '>2</th>
+            <th className='p-2 text-sm border border-white '>3</th>
+            <th className='p-2 text-sm border border-white '>4</th>
+            <th className='p-2 text-sm border border-white '>5</th>
+          </tbody>
+        ))}
+        {/* <tbody>
           <th className='p-2 text-sm border border-white '>1</th>
           <th className='p-2 text-sm border border-white '>2</th>
           <th className='p-2 text-sm border border-white '>3</th>
           <th className='p-2 text-sm border border-white '>4</th>
           <th className='p-2 text-sm border border-white '>5</th>
-        </tbody>
+        </tbody> */}
       </table>
     </div>
   );
