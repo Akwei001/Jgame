77d68ff7f04f3cd2208bdc5492453ab9690bffd3
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2022-10-03 15:34:35 +0100

    cards added to game

--- src/components/MemoryGame.jsx
+++ src/components/MemoryGame.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
+const gameData = [
+  { hiragana: 'あ' },
+  { hiragana: 'い' },
+  { hiragana: 'う' },
+  { hiragana: 'え' },
+  { hiragana: 'お' },
+];
+
+console.log(gameData);
+
 const MemoryGame = () => {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  const shuffleCards = () => {
+    const shuffledCards = [...gameData, ...gameData]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
+  console.log(cards, turns);
+
   return (
     <div>
       <header className='p-6 mb-8'>
@@ -19,12 +44,23 @@ const MemoryGame = () => {
           </div>
         </div>
       </header>
-      <div className='flex'>
-        <h1>Memory Game</h1>
+      <div className=''>
+        <h1 className=''>Memory Game</h1>
         <div className=' p-[2px] rounded-full bg-gradient-to-r from-pink-500  to-purple-500 hover:text-white active:text-opacity-75 focus:outline-none focus:ring'>
-          <button className='block px-8 py-3 text-sm font-medium rounded-full hover:bg-transparent'>
+          <button
+            onClick={shuffleCards}
+            className='block px-2 py-3 text-sm font-medium rounded-full hover:bg-transparent'
+          >
             New Game
           </button>
+        </div>
+        <div className='grid-col-2'>
+          {cards.map((card) => (
+            <div key={card.id}>
+              <div>{card.hiragana}</div>
+              <img src='/img/pngegg.png' />
+            </div>
+          ))}
         </div>
       </div>
     </div>
